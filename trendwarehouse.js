28a983096fd36f77f154832fa05cf7d516f5c1eb
Kajaluxshan7 <88840177+Kajaluxshan7@users.noreply.github.com>
2021-11-14 19:06:59 +0530

    Add files via upload

---
+++ trendwarehouse.js
@@ -0,0 +1,27 @@
+function watch () {
+    alert("Wrist-Watch");
+  }
+function shirt() {
+    alert("Shirt");
+  }
+function tshirt() {
+    alert("T-Shirt");
+  }
+function wallet() {
+    alert("Wallet");
+  }
+function mobile() {
+    alert("Mobile");
+  }
+  function shoe() {
+    alert("Shoe");
+  }
+  function phone() {
+    alert("Iphone 13 Pro Max");
+  }
+function glass() {
+    alert("Sun-Glasses");
+  }
+  function masks() {
+    alert("Mask");
+  }
